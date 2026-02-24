ba5f9f2c7165ecfa09c8a531bd1c167307695626
Philopatair Mallik <philo.n.farid@gmail.com>
2026-02-24 03:44:28 -0800

    Introlab - HelloWorld!

---
+++ Lab/helloWorld.cpp
@@ -0,0 +1,35 @@
+/*
+Author : Philopatair Mallik
+Date   : 02/24/2026 
+purpose: Hello World program 
+*/
+
+//system libraries
+#include <iostream>   //Input/Output library
+#include <cstdlib>    //Random Number Library 
+#include <ctime>      //Time Library
+using namespace std;
+
+//User libraries
+
+//Global Constants - Physics/Chemistry/Conversons/Array Dimensions
+
+//Function Prototype
+
+//Execution begins here
+int main(int argv, char **argc)
+{
+    //Set the Random Number seed
+    srand(static_cast<unsigned int>(time(0)));
+
+    //Declare Variables
+
+    //Initialize Variables
+
+    //Mapping Process -> Inputs to Outputs
+
+    //Display Inputs/Outputs
+    cout<<"Hello World!"<<endl;
+    //Exit the program
+    return 0;
+};
